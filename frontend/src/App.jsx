20ef3fc50842c71fff6cd6b7bd0753b3df7939e2
yogeshchintamani3-coder <yogesh.chintamani@kitaboo.com>
2026-05-06 01:51:17 +0530

    Update API_BASE to live Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { GoogleLogin } from '@react-oauth/google'
 import { jwtDecode } from 'jwt-decode'
 import './App.css'
 
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE = 'https://prompt-enhancer-backend-vfkc.onrender.com/api'
 
 const LANGUAGES = [
   { name: 'English', code: 'en-US' },
